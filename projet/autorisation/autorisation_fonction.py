c3be335a4671ffe6d47d7aad64b8ed03b25613d7
Robin Hotton <60039831+Gerob59@users.noreply.github.com>
2023-05-31 17:18:15 +0200

    fix: role autorisation fonction
    un utilisateur peux avoir plusieurs roles

--- projet/autorisation/autorisation_fonction.py
+++ projet/autorisation/autorisation_fonction.py
@@ -3,8 +3,10 @@ from projet.autorisation.op_colis import fonctions_op_colis
 
 
 def lancer_fonction(utilisateur: UtilisateurSchema, fonction) -> None:
-    role = utilisateur.role
-    if role == "op_colis" and fonction in fonctions_op_colis:
+    roles = utilisateur.roles
+
+    if "op_colis" in roles and fonction in fonctions_op_colis:
         fonction()
     else:
         raise PermissionError("L'utilisateur n'a pas les autorisations nécessaires pour exécuter cette fonction.")
+
